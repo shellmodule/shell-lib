4fb9b372cfbe56f260f843d0f32146dad7fc7982
Cao Xiaoqi <xiaoqi.cao@rea-group.com>
2015-06-15 00:57:54 +0800

    CW-SL-01, Add isEmpty method for array. Cao Xiaoqi

--- test/shell_array_test.sh
+++ test/shell_array_test.sh
@@ -13,4 +13,18 @@ function tearDown()
 	echo "after..."
 }
 
+function testArrayIsEmpty()
+{
+	empty_array=()
+	not_empty_array=("aa","bb")
+
+	_array_is_empty "empty_array"
+	empty_result=$?
+	_array_is_empty "not_empty_array"
+	not_empty_result=$?
+
+	assertEquals "The empty array should be empty" 1 $empty_result
+	assertEquals "The not empty array should not be empty" 0 $not_empty_result
+}
+
 . ../lib/shunit2
